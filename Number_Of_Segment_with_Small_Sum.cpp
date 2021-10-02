4ee5d39f89a076386ed9ea012bfd1f68c7cea51c
NishthaGryffindor <68582856+NishthaGryffindor@users.noreply.github.com>
2021-10-02 17:49:54 +0530

    Two Pointer 3 CF

---
+++ Number_Of_Segment_with_Small_Sum.cpp
@@ -0,0 +1,41 @@
+//Have A Good Day Traveller:)
+//Author: Saksham Goel
+#include<bits/stdc++.h>
+#include<queue>
+#include<vector>
+#define MOD 1000000007
+#define endl "\n"
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+#define int long long int
+#define lli long long int
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x);
+using namespace std;
+
+void solve(){
+	int n,sum,a=0,b;
+	cin>>n>>sum;
+	vector<int> v(n);
+	for(int &i:v)
+		cin>>i;
+	int tsum=0;
+	int count=0;
+	for(int i=0;i<n;i++){
+		tsum+=v[i];
+		while(a<=i and tsum>sum){
+			tsum-=v[a];
+			a++;
+		}
+		count+=(i-a+1);
+	}
+	cout<<count<<endl;
+}
+int32_t main(){
+	fastio;
+//	int t;
+//	cin>>t;
+//	while(t--)
+		solve();
+	return 0;
+}
+
