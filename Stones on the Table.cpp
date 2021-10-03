3233fef32770a2883da265499bb50268ba8119c9
Priyanshu Arora <54005333+PriyanshuArora2001@users.noreply.github.com>
2021-10-03 22:32:26 +0530

    Add files via upload

---
+++ Stones on the Table.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,count=0;
+    cin>>n;
+    string s;
+    cin>>s;
+    for(int i=0;i<s.size()-1;i++)
+    {
+        if(s[i]==s[i+1])
+        {
+            count++;
+        }
+    }
+    cout<<count;
+    return 0;
+}
